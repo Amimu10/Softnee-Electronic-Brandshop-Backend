fb665b6408d4d793bd8a2f5f045e5f1a97664a03
Amimu10 <amimul211@gmail.com>
2023-10-21 00:13:14 +0600

    some changes for deploy issue

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5rlcce.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const productCollection = client.db("productDB").collection("products");
     const cartCollection = client.db("productDB").collection("carts");
 
@@ -118,7 +118,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
