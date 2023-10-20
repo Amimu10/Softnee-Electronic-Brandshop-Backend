1beb5adafe18084234fe7d3f9395b4ded99abdfe
Amimu10 <amimul211@gmail.com>
2023-10-20 18:48:26 +0600

    removing node_modules

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
-
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// Elite-Electro
-// QvJmPFSLI1zgjH8b
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i5rlcce.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,15 +21,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productCollection = client.db("productDB").collection("products");
+    const cartCollection = client.db("productDB").collection("carts");
 
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
       res.send(result);
     });
+
+    app.get("/carts/:user", async (req, res) => {
+      const user = req.params.user;
+      const query = {
+        userName: user,
+      };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cart = req.body;
+      const result = await cartCollection.insertOne(cart)
+  ;
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete", id);
+      const query = {
+        _id: new ObjectId(id)
+  ,
+      };
+      const result = await cartCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
 
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find();
@@ -94,22 +118,11 @@ async function run() {
       res.send(result);
     });
 
-    // Add this route in your server code
-    // app.get("/advertisements", async (req, res) => {
-    //   const cursor = productCollection.find({
-    //     /* Add a condition to filter advertisement items */
-    //   });
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
@@ -122,3 +135,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(` app is running on port: ${port}`);
 });
+
